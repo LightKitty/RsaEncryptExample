a303e10482389ee56d41f22ddee5835c55f21c59
wnmwi <383929371@qq.com>
2019-10-16 20:39:58 +0800

    第一次提交

---
+++ RsaEncryptExample/FormRsaEncrypt.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RsaEncryptExample
+{
+    partial class FormRsaEncrypt
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxKeys = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "原始字符串：";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(110, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(362, 23);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(110, 41);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(100, 25);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "开始加密解密";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "公钥/私钥对：";
+            // 
+            // richTextBoxKeys
+            // 
+            this.richTextBoxKeys.Location = new System.Drawing.Point(110, 72);
+            this.richTextBoxKeys.Name = "richTextBoxKeys";
+            this.richTextBoxKeys.Size = new System.Drawing.Size(362, 96);
+            this.richTextBoxKeys.TabIndex = 4;
+            this.richTextBoxKeys.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "加密后字符串：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "解密后字符串：";
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(110, 200);
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(362, 23);
+            this.textBoxEncrypt.TabIndex = 7;
+            // 
+            // textBoxDecrypt
+            // 
+            this.textBoxDecrypt.Location = new System.Drawing.Point(110, 236);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(362, 23);
+            this.textBoxDecrypt.TabIndex = 8;
+            // 
+            // FormRsaEncrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 276);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBoxKeys);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormRsaEncrypt";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBoxKeys;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+    }
+}
+
---
+++ RsaEncryptExample/FormRsaEncrypt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RsaEncryptExample
+{
+    public partial class FormRsaEncrypt : Form
+    {
+        public FormRsaEncrypt()
+        {
+            InitializeComponent();
+            this.Text = "RSA 加密解密";
+            textBoxEncrypt.ReadOnly = true;
+            textBoxDecrypt.ReadOnly = true;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            //使用默认密钥创建RSACryptoServiceProvider 对象
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            //显示包含公钥/私钥对的XML 表示形式，如果只显示公钥，将参数改为false 即可
+            richTextBoxKeys.Text = rsa.ToXmlString(true);
+            //将被加密的字符串转换为字节数组
+            byte[] dataToEncrypt = Encoding.UTF8.GetBytes(textBoxInput.Text);
+            try
+            {
+                //得到加密后的字节数组
+                byte[] encryptedData = rsa.Encrypt(dataToEncrypt, false);
+                textBoxEncrypt.Text = Encoding.UTF8.GetString(encryptedData);
+                //得到解密后的字节数组
+                byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+                textBoxDecrypt.Text = Encoding.UTF8.GetString(decryptedData);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
+
